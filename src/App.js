aa11a26423e1665a4ac8162377279a132fef9027
Amathia <grosuligor99@mail.ru>
2023-04-24 15:51:46 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 
 import React, {useState} from 'react';
+import { projects } from './components/projects';
+import { PropTypes} from 'prop-types';
 
 
 
@@ -8,81 +10,57 @@ function ProjectList(props) {
   const { projects } = props;
   return(
       <>
-          {projects.map((el) => { return <img src={el.img} alt="el.id"></img>})}
+          {projects.map((el, id) => { return <img key={id} src={el.img} alt="smth"></img>})}
           </>
          
   )
 }
 
 function ToolBar(props) {
-  const {filters } = props;
-  console.log(filters)
+  const {filters} = props;
+
+  function filterSelected(event) {
+    props.onSelectFilter(event.target.innerText)
+
+    }
+  
+
   return (
     <div className='toolbar'>
-      {filters.map((el)=> <button className='btn' key ={el}>{el}</button>)}
+      {filters.map((el)=> <button onClick={filterSelected} className='btn' key ={el}>{el}</button>)}
       
     </div>
   )
 }
 
 function App() {
-
-  const projects = [{
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
-    category: "Business Cards"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
-    category: "Business Cards"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-    category: "Business Cards"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
-    category: "Flayers"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
-    category: "Business Cards"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-    category: "Business Cards"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
-    category: "Websites"
-  }, {
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
-    category: "Flayers"
-  }]
-  const filters=['All', 'Websites', 'Bisness cards', 'Flayers'];
+  
+  const filters=["All", "Websites", "Business Cards", "Flayers"];
 
 
-  const [ selected, setSelected] = useState("All");
+  const [ NewProjects, updateProjects ] = useState(projects)
+
+  const [ selected, setSelected] = useState('All');
+
+function onSelectFilter(filterValue) {
+    setSelected(filterValue)
+    updProjects()
+    }
+
+function updProjects() {
+    updateProjects(projects.filter((item) => {
+      if (selected === 'All') {
+         return projects
+         } else {
+          return item.category === selected
+         }
+        }))
+      }
+
+      onSelectFilter.propTypes = {
+        filterValue: PropTypes.string
+      }
+
 
 
   return (
@@ -90,14 +68,14 @@ function App() {
       <ToolBar
       filters={filters}
       selected={selected}
-      onSelectFilter={(filter)=> {console.log(filter)}}
+      onSelectFilter={onSelectFilter}
        />
        <div className='container'>
+
         <div className='inner'>
-          <ProjectList projects={projects} />
-
-
+          <ProjectList projects={NewProjects} />
         </div>
+
        </div>
 
     </div>
