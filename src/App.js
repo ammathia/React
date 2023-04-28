b12f8b0d4eed9b952f67512a0960ae42b0f1fad7
Amathia <grosuligor99@mail.ru>
2023-04-28 22:14:42 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {useState} from 'react';
 import { projects } from './components/projects';
+import { func } from 'prop-types';
 
 function ProjectList(props) {
   const { projects } = props;
@@ -16,13 +17,16 @@ function ProjectList(props) {
 
 function ToolBar(props) {
   const {filters} = props;
-  const  filterSelected = (event) => {
-    props.onSelectFilter(event.target.innerText)
-    }
+
+
+
+ 
   
   return (
     <div className='toolbar'>
-      {filters.map((el)=> <button onClick={filterSelected} className='btn' key ={el}>{el}</button>)}
+      {filters.map((el)=> <button onClick={event =>{
+        props.onSelectFilter(event.target.innerText);
+        }}  className='btn' key ={el}>{el}</button>)}
       
     </div>
   )
@@ -40,6 +44,7 @@ function App1() {
 function onSelectFilter(filterValue) { 
   setSelected(filterValue);
       }
+      console.log(selected)
 
 const filterProjects = NewProjects.filter((item) => {
   if(selected === 'All') {
@@ -74,11 +79,88 @@ console.log('render')
 }
 
 
-function App() {
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+function Item(props) {
+  const {data} = props;
+
 
   return(
+    <div>
+      { data.showCars ? data.cars.map((item, id)=>{
+        return <div key={id} className='innercontainer1'>
+          <h1 className='triger'>{item.name}</h1>
+          <p style={{margin: "10px"}}>{"year: " + item.year}</p>
+          <input style={{fontSize: "20px", textAlign: "center"}}
+          type="text" onChange={event => {props.ChangeTitle(event)}} />
+
+        </div>
+        
+
+      })
+      :
+      null}
+    </div>
+  )
+}
+
+
+class Title extends React.Component {
+
+
+
+  render() {
+    const {title} = this.props
+    return(
     <>
+    <h1 style={{fontSize: "40px", margin: "10px",}}>{title}</h1>
+    <button onClick={this.props.ToggleFunc}>Toggle</button>
     </>
+  )}
+}
+
+
+function App() {
+
+  const [data, setData] = useState({
+    cars :[{
+    name: "Ford",
+    year: 1998
+  },
+  {
+    name: "Nissan",
+    year: 2004
+  },
+  {
+    name: "Bugatti",
+    year: 1964
+  }],
+  pageTitle: "Cars",
+  showCars: true
+});
+
+function ChangeTitle(event) {
+  
+}
+
+function ToggleFunc() {
+  setData({...data, showCars: !data.showCars})
+  
+
+}
+  return(
+    <div className='container1'>
+     <Title ToggleFunc={ToggleFunc} title={data.pageTitle}></Title>
+      <Item ChangeTitle={ChangeTitle} data={data}/>
+    
+    </div>
   )
 }
 export default App;
