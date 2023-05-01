9b8e92d59f08ab817c30c51513bbffcedc771a38
Amathia <grosuligor99@mail.ru>
2023-05-01 19:23:15 +0300

    1

--- src/App.js
+++ src/App.js
@@ -96,10 +96,11 @@ function Item(props) {
     <div>
       { data.showCars ? data.cars.map((item, id)=>{
         return <div key={id} className='innercontainer1'>
-          <h1 className='triger'>{item.name}</h1>
+          <h1 style={{wordBreak: "break-all"}} className='triger'>Car name: {item.name}</h1>
           <p style={{margin: "10px"}}>{"year: " + item.year}</p>
-          <input style={{fontSize: "20px", textAlign: "center"}}
-          type="text" onChange={event => {props.ChangeTitle(event)}} />
+          <input  style={{fontSize: "20px", textAlign: "center"}}
+          type="text" value={data.cars[id].name}  onChange={event => {props.ChangeTitle(event.target.value, id)}} />
+          <button onClick={()=>props.DeleteItem(id)} style={{margin:"10px", fontSize:"20px"}} type="button">delete</button>
 
         </div>
         
@@ -111,7 +112,6 @@ function Item(props) {
   )
 }
 
-
 class Title extends React.Component {
 
 
@@ -121,15 +121,16 @@ class Title extends React.Component {
     return(
     <>
     <h1 style={{fontSize: "40px", margin: "10px",}}>{title}</h1>
-    <button onClick={this.props.ToggleFunc}>Toggle</button>
+    <button style={{margin: "10px"}} onClick={this.props.ToggleFunc}>Toggle</button>
+    <button onClick={(event)=>this.props.Restart(event)}>Restart</button>
     </>
   )}
 }
 
 
 function App() {
-
-  const [data, setData] = useState({
+  
+  const cars = {
     cars :[{
     name: "Ford",
     year: 1998
@@ -141,28 +142,57 @@ function App() {
   {
     name: "Bugatti",
     year: 1964
-  }],
-  pageTitle: "Cars",
+      }],
+    pageTitle: "Cars",
   showCars: true
-});
+};
 
-function ChangeTitle(event) {
+const startNames = ["Ford", "Nissan", "Bugatti",]
+const [names, setNames] =  useState(startNames);
+const [data, setData] = useState(cars);
   
+function ChangeTitle(name, id) {
+  if (name==="") {
+    name = names[id]
+  }
+ 
+  const car = data.cars[id];
+  car.name = name;
+  const cars = [...data.cars];
+  cars[id] = car;
+  setData({...data, cars})
+
 }
 
 function ToggleFunc() {
   setData({...data, showCars: !data.showCars})
   
+}
+
+function Restart() {
+  setData(cars)
+  setNames(startNames)
+}
+
+function DeleteItem(id) {
+  const cars = [...data.cars];
+  cars.splice(id, 1)
+  setData({...data, cars})
+  const names1 = names;
+  names1.splice(id, 1)
+  setNames(names1)
 
 }
-  return(
-    <div className='container1'>
-     <Title ToggleFunc={ToggleFunc} title={data.pageTitle}></Title>
-      <Item ChangeTitle={ChangeTitle} data={data}/>
+return(
+  <div className='container1'>
+     <Title Restart ={Restart} ToggleFunc={ToggleFunc} title={data.pageTitle}></Title>
+      <Item DeleteItem={DeleteItem} ChangeTitle={ChangeTitle} data={data}/>
     
     </div>
   )
 }
+
+
 export default App;
 
 
